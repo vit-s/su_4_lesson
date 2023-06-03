b9f069fd30b6179a45bc77ccfd0ab92586034356
Vitaly S <32413174+vit-s@users.noreply.github.com>
2023-06-03 23:40:10 +0300

    Next comm

---
+++ index.js
@@ -0,0 +1,26 @@
+// function sayHello (name) {
+//   console.log(`FN => sayHello | EL => ${name}`)
+//   console.log(`Hello => ${name}`)
+//
+// }
+// sayHello(`Sara`)
+//
+// class User {
+//   greet() {
+//     console.log(` Hello word =>> `);
+//
+//   }
+// }
+//
+// let sara = new User()
+// sara.greet()
+// let userData = require('./modules/user')
+// let fs = require('fs')
+// let express = require('express')
+
+
+// console.log(`express =>> `, express)
+
+import users from './modules/user.js'
+
+console.log(`users =>> `, users)
---
+++ modules/user.js
@@ -0,0 +1,51 @@
+// class Person {
+//
+//   constructor(name, age) {
+//     this.name = name
+//     this.age = age
+//   }
+//
+//   hello() {
+//     console.log(`My name is =>> ${this.name}, age ${this.age}`)
+//   }
+//
+// }
+//
+// function sum(a, b) {
+//   console.log(`FN => sum | EL => ${a + b}`)
+//
+// }
+//
+// function power(num, pow) {
+//   console.log(`FN => power | EL => ${Math.pow(num, pow)}`)
+// }
+//
+// let sara = {
+//   name: 'Sara',
+//   age: 20,
+//   status: 'admin'
+// }
+//
+// const PI = 3.14
+//
+// power(`test`)
+// sum(`test`)
+//
+// module.exports = {
+//   Person,
+//   sara,
+//   sum,
+//   PI,
+//   power,
+// }
+//
+//
+let users =
+  [
+    {id: 1, name: 'Vova'},
+    {id: 2, name: 'Sara'},
+    {id: 3, name: 'Mark'},
+    {id: 4, name: 'John'},
+  ]
+
+export default users
